7ae666fcac681f8299e4d3f7ab0c1c54016048b4
nTudor0911 <40658894+ntudor11@users.noreply.github.com>
2019-04-08 11:42:28 +0200

    functional web application
    no login required yet

--- mywebapp/app/Reservation.php
+++ mywebapp/app/Reservation.php
@@ -10,4 +10,8 @@ class Reservation extends Model
     public function client() {
       return $this->belongsTo('App\Client', 'client_id', 'id');
     }
+
+    public function room() {
+      return $this->belongsTo('App\Room', 'room_id', 'id');
+    }
 }
